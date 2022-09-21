f20c4c98b48380d7dde0f1222ead902d7a52fea4
artur_zabrodski <artur.zabrodski@rednavis.com>
2022-09-21 16:37:24 +0300

    update 21/09

--- src/main/chapter6/_1_Inheriting/Zoo.java
+++
@@ -1,4 +0,0 @@
-package main.chapter6._1_Inheriting;
-
-public class Zoo extends java.lang.Object {
-}
---
+++ src/main/chapter6/_1_Understanding_Inheritance/Zoo.java
@@ -0,0 +1,4 @@
+package main.chapter6._1_Understanding_Inheritance;
+
+public class Zoo extends java.lang.Object {
+}
