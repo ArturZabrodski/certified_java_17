343afe2660abc60f46614d9f84bc9176e8ca80b5
artur_zabrodski <artur.zabrodski@rednavis.com>
2022-09-21 17:18:13 +0300

    update 21/09

--- src/main/chapter6/_2_Creating_Classes/Flamingo.java
+++ src/main/chapter6/_2_Creating_Classes/Flamingo.java
@@ -1,15 +1,22 @@
 package main.chapter6._2_Creating_Classes;
 
+// this
 public class Flamingo {
     private String color = null;
 
+    // 1
     public void setColor(String color) {
-        color = color;
+        color = color;      // присваивает значение самому себе
+//        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
     }
 
     public static void main(String... unused) {
         var f = new Flamingo();
         f.setColor("PINK");
-        System.out.print(f.color);
+        System.out.print(f.color);  // выведет null, т.к. в классе нужно присваивать с помощью this
     }
 }
