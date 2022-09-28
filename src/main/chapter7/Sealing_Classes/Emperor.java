7febda93440506a6fcf549423cf0120f0ac21f16
artur_zabrodski <artur.zabrodski@rednavis.com>
2022-09-28 11:34:26 +0300

    update 28.09

---
+++ src/main/chapter7/Sealing_Classes/Emperor.java
@@ -0,0 +1,4 @@
+package main.chapter7.Sealing_Classes;
+
+public final class Emperor extends Penguin {
+}
---
+++ src/main/chapter7/Sealing_Classes/Penguin.java
@@ -0,0 +1,4 @@
+package main.chapter7.Sealing_Classes;
+
+public sealed class Penguin permits Emperor {
+}
